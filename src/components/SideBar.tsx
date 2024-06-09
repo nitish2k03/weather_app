9fd39ba191cc30ac5d74ff3f045d1a95e1ae0add
Nitish <nitish.mahajanhalf8@gmail.com>
2024-06-09 23:49:04 +0530

    Added Darkmode btn + fixed sidebar styling

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import SearchBox from "./SearchBox";
+import DarkModeButton from "./DarkModeButton";
 
 const isCittySame = (city1: ICityFromAPI, city2: ICityFromAPI) => {
   return city1.lat === city2.lat && city1.lon === city2.lon;
@@ -49,61 +50,68 @@ export const SideBar = ({
   };
 
   return (
-    <div className="border-r-2 border-black w-[300px]">
-      <div className="border-b-2 border-black p-4">
-        <SearchBox
-          query={query}
-          setQuery={setQuery}
-          searchedData={data}
-          isLoading={loading}
-          handlePinThisCity={handlePinThisCity}
-        />
-      </div>
-      <div className="h-[78%] flex-col flex px-3 pt-3">
-        <div className="font-bold border-b-2 border-gray-500 pb-2">
-          Pinned Locations
+    <div className="border-r-2 border-black w-[300px] flex flex-col justify-between">
+      <div className="w-full h-[calc(100% - 50px)] flex flex-col">
+        <div className="border-b-2 border-black p-4">
+          <SearchBox
+            query={query}
+            setQuery={setQuery}
+            searchedData={data}
+            isLoading={loading}
+            handlePinThisCity={handlePinThisCity}
+          />
         </div>
-        <div className="flex flex-col text-black">
-          {savedCityCoordinates.map((city, index) => (
-            <div
-              key={`${city.lat}-${index}`}
-              className="flex justify-between items-center bg-gray-300 mt-1 p-2 rounded"
-            >
-              <div>{city.name}</div>
-              <div className="flex text-xs">
-                <button
-                  onClick={() => {
-                    // if city is already visible then remove it
-                    if (isCityInArray(city, visibleCities)) {
-                      setVisibleCities(
-                        visibleCities.filter((item) => item !== city)
+        <div className="h-[78%] flex-col flex px-3 pt-3">
+          <div className="font-bold border-b-2 border-gray-500 pb-2">
+            Pinned Locations
+          </div>
+          <div className="flex flex-col text-black">
+            {savedCityCoordinates.map((city, index) => (
+              <div
+                key={`${city.lat}-${index}`}
+                className="flex justify-between items-center bg-gray-300 mt-1 p-2 rounded"
+              >
+                <div>{city.name}</div>
+                <div className="flex text-xs">
+                  <button
+                    onClick={() => {
+                      // if city is already visible then remove it
+                      if (isCityInArray(city, visibleCities)) {
+                        setVisibleCities(
+                          visibleCities.filter((item) => item !== city)
+                        );
+                      } else {
+                        setVisibleCities([...visibleCities, city]);
+                      }
+                    }}
+                    className="bg-green-500 rounded text-xs px-2 py-1"
+                  >
+                    {/* SHow Hide Icon */}
+                    {isCityInArray(city, visibleCities) ? "Hide" : "Show"}
+                  </button>
+                  <button
+                    className="bg-red-500 rounded text-xs px-2 py-1 ml-2"
+                    onClick={() => {
+                      setSavedCityCoordinates(
+                        savedCityCoordinates.filter((item) => item !== city)
                       );
-                    } else {
-                      setVisibleCities([...visibleCities, city]);
-                    }
-                  }}
-                  className="bg-green-500 rounded text-xs px-2 py-1"
-                >
-                  {/* SHow Hide Icon */}
-                  {isCityInArray(city, visibleCities) ? "Hide" : "Show"}
-                </button>
-                <button
-                  className="bg-red-500 rounded text-xs px-2 py-1 ml-2"
-                  onClick={() => {
-                    setSavedCityCoordinates(
-                      savedCityCoordinates.filter((item) => item !== city)
-                    );
-                  }}
-                >
-                  Delete
-                </button>
+                    }}
+                  >
+                    Delete
+                  </button>
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       </div>
-      <div className="border-t-2 border-black px-2">
-        <RealTimeClock />
+      <div className="border-t-2 h-[50px] border-black px-2 flex items-center">
+        <div className="flex w-2/3 justify-center items-center">
+          <RealTimeClock />
+        </div>
+        <div className="flex w-1/3 justify-end">
+          <DarkModeButton />
+        </div>
       </div>
     </div>
   );
