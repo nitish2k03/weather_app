52603e3739c736b814f1f9189f368c766bb2d1a6
Nitish <nitish.mahajanhalf8@gmail.com>
2024-06-09 16:14:14 +0530

    removed Navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-// import Navbar from "../components/Navbar";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -18,15 +17,6 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         {/* <Navbar /> */}
-        <div>
-          <div className=" bg-black text-white flex items-center justify-around p-3">
-            <div>WEATHER</div>
-            <div
-              id={"SearchBarPortal"}
-              className="Search-bar-portal w-[500px]"
-            ></div>
-          </div>
-        </div>
         {children}
       </body>
     </html>
